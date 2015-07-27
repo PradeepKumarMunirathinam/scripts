b37d16dbe0d92c078465ffacec86d280c8f80f45
PreetiCK <PreetiCK@users.noreply.github.com>
2015-07-27 17:56:47 +0530

    Update business_conf.sh

--- business_conf.sh
+++ business_conf.sh
@@ -2,4 +2,8 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #Run Docker
+docker rm vapp
+docker rmi preetick/business:latest
 docker run --name vapp -d -p 8080:8080 -e DB_IP=dbvminstance.westus.cloudapp.azure.com -e DB_PASSWORD=welcome123 -e DB_PORT=3306 -e DB=VehicleRental preetick/business:latest
+
+docker restart vapp

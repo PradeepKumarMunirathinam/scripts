0cecd42fe418138481f8a68295703cb236799ebe
PreetiCK <PreetiCK@users.noreply.github.com>
2015-07-28 18:01:46 +0530

    Update presentation_conf.sh

--- presentation_conf.sh
+++ presentation_conf.sh
@@ -6,5 +6,5 @@ wget -qO- https://get.docker.com/ | sh
 docker stop webp
 docker rm webp
 docker rmi  preetick/presentation:latest
-docker run --name webp -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.southeastasia.cloudapp.azure.com preetick/presentation:latest
-docker restart webp
+docker run --name web -d -p 8080:8080 -e BUSINESS_IP=businessvminstance.southeastasia.cloudapp.azure.com preetick/presentation:latest
+docker restart web

d367395b3908e75b774e9165dac8f20b97721f77
preetick <preetikhodanpur038@gmail.com>
2015-12-13 18:35:23 +0530

    ada

--- apex/install_javatomcat.sh
+++ apex/install_javatomcat.sh
@@ -1,18 +1,18 @@
 
 # Install Java
-sudo  apt-get  update
+sudo  apt-get -y update
 
 
 
-sudo  apt-get install  tar
-sudo  apt-get install  unzip
+sudo  apt-get -y install  tar
+sudo  apt-get -y install  unzip
 
 #install java
 
-sudo  add-apt-repository  ppa:webupd8team/java
+sudo  add-apt-repository -y  ppa:webupd8team/java
 
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo  apt-get  install oracle-java8-installer
+sudo  apt-get  -y install oracle-java8-installer
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
 export JRE_HOME=/usr/lib/jvm/java-8-oracle/jre/
 

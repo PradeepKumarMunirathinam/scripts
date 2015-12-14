46a967e1919d5ae1afb3e668046116d97507a7a1
preetick <preetikhodanpur038@gmail.com>
2015-12-14 20:42:45 +0530

    asws

--- apex/install_r.sh
+++ apex/install_r.sh
@@ -6,6 +6,7 @@ sudo  apt-get -y install  unzip
 #install java
 sudo  add-apt-repository -y  ppa:webupd8team/java
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get -f install
 sudo  apt-get  -y install oracle-java8-installer
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
 export JRE_HOME=/usr/lib/jvm/java-8-oracle/jre/

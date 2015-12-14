7508caafb6226ce9d07ba972e2598d0446cac0b5
preetick <preetikhodanpur038@gmail.com>
2015-12-14 14:42:25 +0530

    casc

--- apex/install_javatomcat.sh
+++ apex/install_javatomcat.sh
@@ -9,17 +9,6 @@ echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | su
 sudo  apt-get  -y install oracle-java8-installer
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
 export JRE_HOME=/usr/lib/jvm/java-8-oracle/jre/
-#install tomcat
-sudo mkdir /opt/tomcat
-cd /opt/tomcat
-sudo wget http://www.interior-dsgn.com/apache/tomcat/tomcat-8/v8.0.29/bin/apache-tomcat-8.0.29.zip
-sudo wget https://www.apache.org/dist/tomcat/tomcat-8/v8.0.29/bin/apache-tomcat-8.0.29.zip.md5
-cat apache-tomcat-8.0.29.zip.md5
-md5sum apache-tomcat-8.0.29.zip
-sudo unzip apache-tomcat-8.0.29.zip
-cd apache-tomcat-8.0.29/bin
-sudo chmod 777 /opt/tomcat/apache-tomcat-8.0.29/bin/*.sh
-sudo ln -s /opt/tomcat/apache-tomcat-8.0.29/bin/startup.sh /usr/bin/tomcatup
- sudo ln -s /opt/tomcat/apache-tomcat-8.0.29/bin/shutdown.sh /usr/bin/tomcatdown
-sudo chmod 777 /opt/tomcat/apache-tomcat-8.0.29/logs/
-tomcatup
+
+sudo wget -nv http://dl.bintray.com/rundeck/rundeck-deb/rundeck-2.5.1-1-GA.deb -O /tmp/rundeck.deb
+sudo dpkg -i /tmp/rundeck.deb && rm /tmp/rundeck.deb

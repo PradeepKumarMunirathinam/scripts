df64f72c8ea778412c9f42f494a6a212e037bbe6
preetick <preetikhodanpur038@gmail.com>
2015-12-14 11:27:02 +0530

    sakd

---
+++ apex/install_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get -y update
+
+sudo  apt-get -y install  tar
+sudo  apt-get -y install  unzip
+#install java
+sudo  add-apt-repository -y  ppa:webupd8team/java
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo  apt-get  -y install oracle-java8-installer
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
+export JRE_HOME=/usr/lib/jvm/java-8-oracle/jre/
+
+#rundeck
+sudo wget -nv http://dl.bintray.com/rundeck/rundeck-deb/rundeck-2.5.1-1-GA.deb -O /tmp/rundeck.deb
+sudo dpkg -i /tmp/rundeck.deb && rm /tmp/rundeck.deb

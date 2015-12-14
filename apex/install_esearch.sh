e81bdbb2fb8364b388be1e947496694328efbbf3
preetick <preetikhodanpur038@gmail.com>
2015-12-14 15:53:25 +0530

    hvh

--- apex/install_esearch.sh
+++ apex/install_esearch.sh
@@ -10,7 +10,7 @@ sudo  apt-get  -y install oracle-java8-installer
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
 export JRE_HOME=/usr/lib/jvm/java-8-oracle/jre/
 
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key -y add -
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 
 sudo apt-get -y update
